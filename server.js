95d82f261a52801d76963fae44e92297ce6ddded
FreakVLive <3kafadar87@gmail.com>
2025-11-25 22:23:19 +0300

    Güncelleme işte amk

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log('server.js is being executed');
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Merhaba, Berber Panel!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server ${PORT} portunda çalışıyor`));
